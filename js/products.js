21e7e81d6d80868e5a6b52d49e60d81835ea828b
giannabarrios <giannabarrios012@gmail.com>
2022-08-16 23:09:10 -0300

    Modificaciones en listado de productos

--- js/products.js
+++ js/products.js
@@ -1,30 +1,30 @@
-let categoriesArray = [];
+let productsArray = [];
 
-function showCategoriesList(array){
+function showProductsList(array){
     let htmlContentToAppend = "";
 
     for(let i = 0; i < array.products.length; i++){ 
-        let category = array.products[i];
+        let product = array.products[i];
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + category.image + `" alt="product image" class="img-thumbnail">
+                    <img src="` + product.image + `" alt="product image" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <div class="mb-1">
-                        <h4>`+ category.name + ` - `+ category.currency +` `+ category.cost + `</h4> 
-                        <p> `+ category.description +`</p> 
+                        <h4>`+ product.name + ` - `+ product.currency +` `+ product.cost + `</h4> 
+                        <p> `+ product.description +`</p> 
                         </div>
-                        <small class="text-muted">` + category.soldCount + ` vendidos</small> 
+                        <small class="text-muted">` + product.soldCount + ` vendidos</small> 
                     </div>
 
                 </div>
             </div>
         </div>
         `
-        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend; 
     }
 }
 
@@ -33,8 +33,8 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok")
         {
-            categoriesArray = resultObj.data;
-            showCategoriesList(categoriesArray);
+            productsArray = resultObj.data;
+            showProductsList(productsArray);
         }
     });
 });
